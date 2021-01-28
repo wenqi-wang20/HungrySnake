eadc260583e09c0e2bfa05e2e03cb8159418602c
wenqi-wang20 <1974905835@qq.com>
2021-01-28 23:36:20 +0800

    2021.1.28.23.40

---
+++ Map.hpp
@@ -0,0 +1,16 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include<vector>
+#include"Points.hpp"
+class Map
+{
+public:
+    Map(){
+        initmap.emplace_back(Point(1,1));
+
+    }
+private:
+    
+};
+#endif
